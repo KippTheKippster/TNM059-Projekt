a0ce8a8ddf5d82a2f63c46137ead80c82ce691f4
KippTheKippster <43861443+KippTheKippster@users.noreply.github.com>
2024-04-09 22:07:03 +0200

    Add Fps counter

--- player/player_ui.gd
+++ player/player_ui.gd
@@ -3,6 +3,8 @@ extends Control
 @export var player: Player
 
 @onready var health_bar: ProgressBar = %HealthBar
+@onready var fps_label: Label = $FPSLabel
 
 func _process(delta: float):
 	health_bar.value = player.health_area.health
+	fps_label.text = str(Engine.get_frames_per_second())
