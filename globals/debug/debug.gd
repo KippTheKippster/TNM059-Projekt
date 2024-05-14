8e2c3c4bacf653621329781314d0d5f2b8c37150
KippTheKippster <43861443+KippTheKippster@users.noreply.github.com>
2024-05-14 14:25:30 +0200

    Add pause menu

--- globals/debug/debug.gd
+++ globals/debug/debug.gd
@@ -12,3 +12,9 @@ func _process(_delta: float) -> void:
 		Engine.time_scale = 3.0
 	elif Input.is_action_just_released("fast_forward"):
 		Engine.time_scale = 1.0
+	
+	if Input.is_action_just_pressed("fullscreen"):
+		if DisplayServer.window_get_mode() != DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
---
+++ main/game/pause_menu.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+
+func _ready() -> void:
+	visible = false
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		get_tree().paused = !get_tree().paused
+		visible = get_tree().paused
+	
