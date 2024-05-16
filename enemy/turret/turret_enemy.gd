29e81ccbe4c9b30a86665a740bd2eb3933818162
KippTheKippster <43861443+KippTheKippster@users.noreply.github.com>
2024-05-16 08:30:35 +0200

    Fix janky rotations for turret

--- enemy/turret/turret_enemy.gd
+++ enemy/turret/turret_enemy.gd
@@ -11,22 +11,38 @@ const ENEMY_BULLET = preload("res://enemy/enemy_bullet.tscn")
 @onready var awake: AtomicState = $StateChart/Active/Awake
 @onready var sattelite: Node3D = $Sattelite
 
+@export var debug: bool = false
+
 @onready var head: Node3D = $Sattelite/Head
 
-func _ready() -> void:
-	sattelite.top_level = true
-	sattelite.global_rotation = global_rotation
-	sattelite.global_position = global_position
+#func _ready() -> void:
+#	head.top_level = true
+#	head.global_rotation = global_rotation
+#	head.global_position = global_position
 
 func _process(delta: float) -> void:
 	var dif := player.global_position - global_position
-
+	dif = global_basis.inverse() * dif
+	
 	var horizontal := Vector2(dif.z, dif.x)
 	var vertical := Vector2(dif.y, horizontal.length())
 	
 	var angle_x := clamp(vertical.angle(), -PI / 2, PI / 2) as float
+	#angle_x = vertical.angle()
 	
 	head.rotation = Vector3(angle_x, horizontal.angle(), 0)
+	
+	if debug:
+		print("local:")
+		print_angle(head.rotation)
+		print("global:")
+		print_angle(head.global_rotation)
+	#head.rotation.x = clamp(head.rotation.x, -PI / 4, PI / 4)
+	#if debug:
+	#	print("cur:", rad_to_deg(head.rotation.x))
+
+func print_angle(v: Vector3) -> void:
+	print(rad_to_deg(v.x), ", ", rad_to_deg(v.y), ", ", rad_to_deg(v.z))
 
 func shoot() -> void:
 	for i in shoot_amount:
